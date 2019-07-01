eecc5afb4499d1138284088a172a897facb73a1d
geraintwilliams <geraint.williams@gmail.com>
2019-07-01 15:42:34 +0100

    add test file

--- test/bob/core_test.clj
+++ test/bob/core_test.clj
@@ -2,6 +2,28 @@
   (:require [clojure.test :refer :all]
             [bob.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest simple-tests
+    (is (= (to-numerals 1) "I"))
+    (is (= (to-numerals 5) "V"))
+    (is (= (to-numerals 88) "LXXXVIII"))
+    (is (= (to-numerals 1712) "MDCCXII"))
+    (is (= (to-numerals 2000) "MM"))
+    (is (= (to-numerals 2029) "MMXXIX"))
+    )
+
+(deftest boundery-tests
+    (is (= (to-numerals 0) ""))
+    )
+
+(deftest internal-to-factors
+  (is (= (to-factors 12 [10 1]) [1 2]))
+  (is (= (to-factors 12 [8 4 1]) [1 1 0]))
+  (is (= (to-factors 100 [64 32 16 8 4 2 1]) [1 1 0 0 1 0 0 ]))
+  (is (= (to-factors 99 [50 40 10 9 5 4 1]) [1 1 0 1 0 0 0]))
+  )
+
+(deftest internal-expand-to-string
+  (is (= (expand-to-string [["x" 10]]) "xxxxxxxxxx"))
+  (is (= (expand-to-string [["x" 2] ["y" 2]]) "xxyy"))
+  (is (= (expand-to-string [["bob" 2] ["bad" 0] ["fred" 1] ["bad" 0]]) "bobbobfred"))
+  )
